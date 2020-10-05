5cc353c04924564652cd327cd5a2fbf528efb041
Dev-Shankar-Paul <paulds2001@gmail.com>
2020-10-06 01:01:52 +0530

    Moved Datasets into python codes

---
+++ Python_Codes/Dataset_open_trial.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 21:53:27 2020
+
+@author: bambo
+"""
+import pandas as pd
+import os
+
+loc = os.getcwd() + r'/final_new_crop_data_own_repeat.csv'
+table = pd.read_csv(loc)
+
