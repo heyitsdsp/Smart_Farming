373e8cd61bad99221e9c29d23814c729f52515ad
Dev-Shankar-Paul <paulds2001@gmail.com>
2020-10-14 13:35:05 +0530

    uncommented the new changes

--- Python_Codes/Dataset_open_trial.py
+++ Python_Codes/Dataset_open_trial.py
@@ -9,7 +9,6 @@ Created on Mon Oct  5 21:53:27 2020
 import pandas as pd
 import numpy as np
 import os
-import re    
 
 # PICK AND READ DATASET DYNAMICALLY 
 loc = os.getcwd() + r'/Datasets/Crop_Pred.csv'
@@ -27,36 +26,21 @@ for char in spec_chars:
        table['Crop'] = table['Crop'].str.replace(char, ' ')
        table['Crop'] = table['Crop'].str.replace(r"\(.*\)","")
 
-# FIX MORINGA ISSUE IN A COMPLICATED MANNER
-"""def moringa(crop_name):
-    pattern = re.compile(r'(Drumstick)')
-    drumsticks = re.findall(pattern, crop_name)
-    new_name = "".join(drumsticks)
-    return new_name
-
-i = 0      
-
-for crop in table['Crop']:
-    if(crop == "Drumstick   moringa"):
-        table['Crop'][i] = moringa(crop)
-    i+=1
-"""
-
 # FIX MORINGA ISSUE IN A SIMPLE MANNER
-"""i = 0
+i = 0
 for i in range(0, len(table['Crop'])):
     if(table.Crop[i] == "Drumstick   moringa"):
         table.Crop[i] = "Drumstick"
     else:
         continue
-"""
 
-# SCALING USING NUMPY
-"""
+
+# INTERPOLATION USING NUMPY
+
 table.N = table.N.astype('float64')
 for i in range(0, len(table.N)):
-    table.N[i] = round(np.interp(table.N[i], [10, 180], [0, 1]), 2)
-""" 
-    
+    table.N[i] = round(np.interp(table.N[i], [10, 180], [0, 1]), 1)
+
+  
         
     
