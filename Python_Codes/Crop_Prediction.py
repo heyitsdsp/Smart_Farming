2462725e47a6b42670fc5be258dafe714858d23a
Dev-Shankar-Paul <paulds2001@gmail.com>
2020-10-16 13:11:44 +0530

    tested 3 models

--- Python_Codes/Crop_Prediction.py
+++ Python_Codes/Crop_Prediction.py
@@ -8,7 +8,7 @@ import Dataset as ds
 import Sensor_values as sv
 import os
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+import numpy as np
 
 # GET THE DATASET INTO THE CODE
 loc = os.getcwd() + r'/Datasets/Crop_Pred.csv'
@@ -27,7 +27,56 @@ X = dataset.iloc[:, 1:].values
 Y = dataset.iloc[:, 0].values
 
 # SPLIT X AND Y INTO THE TRAINING SET AND TEST SET
+from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2,
                                                     random_state = 0)
+"""
+# MULTIPLE LINEAR REGRESSION
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+Y_pred = regressor.predict(X_test)
+np.set_printoptions(precision = 2)
+print(np.concatenate((Y_pred.reshape(len(Y_pred), 1), 
+                      Y_test.reshape(len(Y_pred), 1)), axis = 1))
+"""
+
+"""
+# PLOYNOMIAL REGRESSION
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+# Go for a polynomial of degree 4 but 20 is good as well
+poly_reg = PolynomialFeatures(degree = 4)
+X_train_poly = poly_reg.fit_transform(X_train)
+poly_model = LinearRegression()
+poly_model.fit(X_train_poly, Y_train)
+
+Y_pred = poly_model.predict(poly_reg.fit_transform(X_test))
+for i in range(0, len(Y_pred)):
+    Y_pred[i] = round(Y_pred[i])
+    
+Y_pred = Y_pred.astype('int')
+np.set_printoptions(precision = 2)
+print(np.concatenate((Y_pred.reshape(len(Y_pred), 1), 
+                      Y_test.reshape(len(Y_pred), 1)), axis = 1))    
+"""
+
+"""
+# BAYESIAN RIDGE REGRESSION
+from sklearn.linear_model import BayesianRidge
+bay_ridge = BayesianRidge()
+bay_ridge.fit(X_train, Y_train)
+Y_pred = bay_ridge.predict(X_test)
+
+np.set_printoptions(precision = 2)
+print(np.concatenate((Y_pred.reshape(len(Y_pred), 1), 
+                      Y_test.reshape(len(Y_pred), 1)), axis = 1))
+"""
+
+# ARTIFICIAL NEURAL NETWORK USING TENSORFLOW 2.0
+import tensorflow as tf
+
 
 
